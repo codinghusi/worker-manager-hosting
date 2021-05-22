321946e262d3de0e7222cad647d383e1da50787c
Gerrit <gerrit.weiermann@gmx.de>
2021-05-22 17:34:36 +0200

    wip

--- dgraph/init.sh
+++ dgraph/init.sh
@@ -5,7 +5,7 @@ check_file=/dgraph/initialized.txt
 while [ ! -d "$check_file" ];
 do
     sleep 10s
-    if curl -X POST 'http://localhost:8080/admin/schema' --data-binary '@database.graphql' | grep 'Success'
+    if curl -X POST 'http://localhost:8080/admin/schema' --data-binary '@schema.graphql' | grep 'Success'
     then
         echo "\n------------------ initialized graphql schema ------"
         echo "finished" > $check_file
