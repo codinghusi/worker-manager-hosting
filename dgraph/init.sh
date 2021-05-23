9b66fa5ea8c07cd376b578588c17c76a5eb646aa
Gerrit <gerrit.weiermann@gmx.de>
2021-05-23 12:58:14 +0200

    wip

--- dgraph/init.sh
+++ dgraph/init.sh
@@ -4,11 +4,13 @@ check_file=/dgraph/initialized.txt
 
 while [ ! -d "$check_file" ];
 do
-    sleep 10s
+    sleep 30s
     if curl -X POST 'http://localhost:8080/admin/schema' --data-binary '@schema.graphql' | grep 'Success'
     then
         echo "\n------------------ initialized graphql schema ------"
         echo "finished" > $check_file
         break
     fi
+
+    break # somehow checking for 'Success' doesn't work anymore
 done
